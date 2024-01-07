cdfe3460d322c3f415ae1db08f48932e328078ed
Runner-Dad84 <vanhoogie@gmail.com>
2024-01-06 22:17:31 -0500

    API promise to fetch current weather for London.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Fetch data from WeatherAPI
+try{
+fetch('http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=London&aqi=no', {mode: "cors"})
+ .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+  });
+} catch {
+    console.log('error')
+}
+
+
+//Test function
 function testBtn () {
  console.log('This is a test of the public emergency line! It is a drill.');
 };
