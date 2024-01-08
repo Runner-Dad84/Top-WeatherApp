21e8979c4e1c240d3714e14efedcd5133f3f4a2d
Runner-Dad84 <vanhoogie@gmail.com>
2024-01-07 22:02:23 -0500

    Search by city name for current weather.

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-//City Input
-let cityLoc = document.getElementById('city-loc');
+
 const submitBtn = document.getElementById('submit');
-submitBtn.addEventListener('click', ()=> {
-    console.log("button pressed");
-})
 
+async function submitData () {
+    let input = document.getElementById('locCity');
+    const address = `http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=${input.value}&aqi=no`
+    console.log(input.value);
+    try{
+        const response = await fetch(address, {mode: "cors"});
+        const responseJson = await response.json();
+        console.log(responseJson);
+        } catch {
+            console.log('error')
+        }
 
-//Fetch data from WeatherAPI
-try{
-fetch('http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=London&aqi=no', {mode: "cors"})
- .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-  });
-} catch {
-    console.log('error')
 }
+
