8c1d883b16fb8b91c96dea9784d2eeb875adcb4f
Runner-Dad84 <vanhoogie@gmail.com>
2024-01-07 22:12:23 -0500

    Add name and message to catch

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-const submitBtn = document.getElementById('submit');
+
 
 async function submitData () {
     let input = document.getElementById('locCity');
     const address = `http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=${input.value}&aqi=no`
-    console.log(input.value);
+    
     try{
         const response = await fetch(address, {mode: "cors"});
         const responseJson = await response.json();
         console.log(responseJson);
-        } catch {
-            console.log('error')
+        } catch (err) {
+            console.log(err.name);
+            console.log(err.message);
         }
 
 }
-
