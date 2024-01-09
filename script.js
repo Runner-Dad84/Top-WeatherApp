86c202c5f57e38a064c5414a3131b18c78a11591
Runner-Dad84 <vanhoogie@gmail.com>
2024-01-08 21:33:56 -0500

    Pull and assign data to variables. Add error to response.

--- script.js
+++ script.js
@@ -2,16 +2,28 @@
 
 
 async function submitData () {
+    const boxTemp = document.getElementById('boxTemp');
     let input = document.getElementById('locCity');
     const address = `http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=${input.value}&aqi=no`
     
     try{
         const response = await fetch(address, {mode: "cors"});
-        const responseJson = await response.json();
-        console.log(responseJson);
-        } catch (err) {
+         if (!response .ok) {
+            throw new Error(`http error status:${response.status}`);
+         }
+
+        const respJson = await response.json();
+        console.log(respJson);
+        
+        const temp = await(respJson.current.temp_f);
+        const feels = await(respJson.current.feelslike_f);
+        const wind = await(respJson.current.wind_mph);
+        const condition = await(respJson.current.condition.text);
+        console.log(temp, feels, wind, condition);
+        return(temp, feels, wind, condition);
+    } catch (err) {
             console.log(err.name);
             console.log(err.message);
-        }
+    }   
 
-}
+};
