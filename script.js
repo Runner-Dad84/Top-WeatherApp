23bcdfa26e9fd49604ab218f6c377ec9d51c2153
Runner-Dad84 <vanhoogie@gmail.com>
2024-01-09 21:35:24 -0500

    Add image with condition variable as search term. Add default location NYC.

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const boxCondition = document.getElementById('boxCondition');
 const boxTemp = document.getElementById('boxTemp');
 const boxFeels = document.getElementById('boxFeels');
 const boxWind = document.getElementById('boxWind');
+const img = document.querySelector('img');
+let input = document.getElementById('locCity');
+
 
 async function submitData () {
-    let input = document.getElementById('locCity');
     const address = `http://api.weatherapi.com/v1/current.json?key=1abc13cc84da46599bb30023240701&q=${input.value}&aqi=no`
     
     try{
@@ -27,18 +29,23 @@ async function submitData () {
         console.log(city, region, temp, feels, wind, condition);
         
         boxLoc.innerText = `${city}, ${region}`;
-        boxCondition.innerText = `Current Weather: ${condition}`;
+        boxCondition.innerText = `Current Conditions: ${condition}`;
         boxTemp.innerText = `Temperature: ${temp} F`;
         boxFeels.innerText = `Feels Like: ${feels} F`;
         boxWind.innerText = `Wind: ${wind} mph`;
-        
 
+        const image = `https://api.giphy.com/v1/gifs/translate?api_key=AlCs3pB2zAZb4rdwwi95WBUlExWsY7ed&s=${respJson.current.condition.text}`
+        const responseImage = await fetch(image, {image: 'cors'})
+        const imageJson = await responseImage.json();
+        img.src = await imageJson.data.images.original.url;
+        console.log(respJson.current.condition.text);
+   
     } catch (err) {
             console.log(err.name);
             console.log(err.message);
     }   
-
 };
-
-
-
+(function defaultLocation () {
+    input.value = "New York";
+    submitData();
+})();
